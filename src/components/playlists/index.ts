2174033b9345ecb56350d842d52c6cb6e4f65ee4
linder3hs <linderhassinger00@gmail.com>
2023-01-30 17:22:22 -0500

    feat: add delete

--- src/components/playlists/index.ts
+++ src/components/playlists/index.ts
@@ -6,5 +6,6 @@ const playlistRouter: Router = Router();
 playlistRouter.get("/:id", Controller.findAll);
 playlistRouter.post("/", Controller.store);
 playlistRouter.put("/:id", Controller.update);
+playlistRouter.delete("/:id", Controller.destroy);
 
 export default playlistRouter;
